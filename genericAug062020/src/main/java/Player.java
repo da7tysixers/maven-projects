7ca5def6c592dfab2ccd8a523784aac78d234b37
Emmanuel Orubele <blontf@gmail.com>
2020-08-06 22:45:46 -0400

    Added generics to maven project

---
+++ genericAug062020/src/main/java/Player.java
@@ -0,0 +1,13 @@
+ abstract public class Player {
+    private String teamName;
+
+     public Sports(String teamName) {
+         this.teamName = teamName;
+     }
+
+     public String getTeamName() {
+         return teamName;
+     }
+
+   
+ }
