5056be20217348462cfb20ea51f0386f9881751b
Emmanuel Orubele <blontf@gmail.com>
2020-08-22 15:20:24 -0400

    Collections List

--- CollectionsAug192020/src/main/java/MainApplication.java
+++ CollectionsAug192020/src/main/java/MainApplication.java
@@ -4,6 +4,17 @@ public class MainApplication {
 
     public static void main(String[] args) {
        Theatre theatre = new Theatre("Olympian", 8, 12);
-       theatre.getSeats();
+        theatre.getSeats();
+       if (theatre.reserveSeat("H11")){
+           System.out.println("Please pay");
+       }else {
+           System.out.println("Seat taken" );
+       }
+
+        if (theatre.reserveSeat("H11")){
+            System.out.println("Please pay");
+        }else {
+            System.out.println("Seat taken" );
+        }
     }
 }
--- CollectionsAug192020/src/main/java/Theatre.java
+++ CollectionsAug192020/src/main/java/Theatre.java
@@ -1,15 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
+import javafx.print.Collation;
+
+import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<Seat>();
+    private Collection<Seat> seats = new LinkedHashSet<Seat>() ;
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
 
 
-        int lastRow = 'A' + (numRows + 1);
+        int lastRow = 'A' + (numRows - 1);
         for (char row = 'A'; row <= lastRow; row++) {
             for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
                 Seat seat = new Seat(row + String.format("%02d", seatNum));
@@ -31,11 +32,12 @@ public class Theatre {
                 requestSeat = seat;
                 break;
             }
+        }
             if (requestSeat == null) {
                 System.out.println("There is no more seat " + seatNumber);
                 return false;
             }
-        }
+
         return requestSeat.reserve();
     }
 
