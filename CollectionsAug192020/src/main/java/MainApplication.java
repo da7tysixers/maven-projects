663dc895ad513b9ba3d6690c524bb824a8146fd4
Emmanuel Orubele <blontf@gmail.com>
2020-08-23 00:17:30 -0400

    Binary Collections

--- CollectionsAug192020/src/main/java/MainApplication.java
+++ CollectionsAug192020/src/main/java/MainApplication.java
@@ -1,20 +1,26 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainApplication {
 
     public static void main(String[] args) {
        Theatre theatre = new Theatre("Olympian", 8, 12);
-//        theatre.getSeats();
-       if (theatre.reserveSeat("D13")){
-           System.out.println("Please pay");
-       }else {
-           System.out.println("Seat taken" );
-       }
-//
-//        if (theatre.reserveSeat("H11")){
-//            System.out.println("Please pay");
-//        }else {
-//            System.out.println("Seat taken" );
-//        }
+       List<Theatre.Seat> seatCopy = new ArrayList<Theatre.Seat>(theatre.seats);
+       printList(seatCopy);
+        Collections.shuffle(theatre.seats);
+        printList(theatre.seats);
+        Theatre.Seat max = Collections.max(theatre.seats);
+        System.out.println(max.getSeatNumber());
+
+
+    }
+
+    public static void printList(List<Theatre.Seat> list){
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("===============================================");
     }
 }
