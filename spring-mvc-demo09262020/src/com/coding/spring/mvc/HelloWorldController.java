a7260e3590ae7d9ff6fb33d9576cc1bd05cc4f9e
Emmanuel Orubele <blontf@gmail.com>
2020-10-07 19:37:55 -0400

    added showform

--- spring-mvc-demo09262020/src/com/coding/spring/mvc/HelloWorldController.java
+++ spring-mvc-demo09262020/src/com/coding/spring/mvc/HelloWorldController.java
@@ -1,5 +1,23 @@
 package com.coding.spring.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class HelloWorldController {
+	
+	//need a controller method to show initial form
+	@RequestMapping(value = "/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	
+	// need a controller method to process the form
+	@RequestMapping(value = "/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
 
 }
