0c957732b4a55bf932f72cc4bd18ab155055028a
Emmanuel Orubele <blontf@gmail.com>
2020-10-31 16:54:33 -0400

    added Monkey class

---
+++ algorithm-data-structure/src/datastructure/animal/Monkey.java
@@ -0,0 +1,23 @@
+package datastructure.animal;
+
+public class Monkey implements Animal {
+
+	@Override
+	public void age() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sound() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
